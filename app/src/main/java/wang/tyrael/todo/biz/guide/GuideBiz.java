9ab920ab15fc8d04003ab21dd76a1164f7a8dd0c
王超 <343331640@qq.com>
2017-07-07 12:33:59 +0800

    初始化数据， 应用介绍

--- app/src/main/java/wang/tyrael/todo/biz/guide/GuideBiz.java
+++ app/src/main/java/wang/tyrael/todo/biz/guide/GuideBiz.java
@@ -1,5 +1,6 @@
 package wang.tyrael.todo.biz.guide;
 
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.csmall.android.PreferenceHelper;
 import com.example.avjindersinghsekhon.minimaltodo.ToDoItem;
 
@@ -42,22 +43,27 @@ public class GuideBiz {
         List<ToDoItem> list = new ArrayList<>();
         ToDoItem toDoItem = new ToDoItem();
         toDoItem.setToDoText("别忘了还信用卡，别忘了预约，别忘了买票等等。用笔头把他们记下来，每天看看还有什么事没做。");
+        toDoItem.setTodoColor(ColorGenerator.MATERIAL.getRandomColor());
         list.add(toDoItem);
 
         toDoItem = new ToDoItem();
         toDoItem.setToDoText("试一试：拖动可以交换顺序");
+        toDoItem.setTodoColor(ColorGenerator.MATERIAL.getRandomColor());
         list.add(toDoItem);
 
         toDoItem = new ToDoItem();
         toDoItem.setToDoText("试一试：按住向右滑，再快速往下扔，可以把某一条仍在列表最后");
+        toDoItem.setTodoColor(ColorGenerator.MATERIAL.getRandomColor());
         list.add(toDoItem);
 
         toDoItem = new ToDoItem();
         toDoItem.setToDoText("试一试：右滑完成：事情做完了");
+        toDoItem.setTodoColor(ColorGenerator.MATERIAL.getRandomColor());
         list.add(toDoItem);
 
         toDoItem = new ToDoItem();
         toDoItem.setToDoText("试一试：左滑删除：事情取消了");
+        toDoItem.setTodoColor(ColorGenerator.MATERIAL.getRandomColor());
         list.add(toDoItem);
 
         return list;
